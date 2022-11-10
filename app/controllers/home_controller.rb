1988b1bfa9b16eb7865c8e7dcc405a213c3e1b7e
Lautaro Torchia <torchia.lautaro2@gmail.com>
2022-11-10 12:11:07 -0300

    change of styles

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,12 @@
+class HomeController < ApplicationController
+
+    def index 
+    end
+
+    def about
+        @about_me = "Este es el banco de Ruby on Rails"
+    end
+
+
+    
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   resources :branches do
     resources :schedules
   end
+  root 'home#index'
+  get '/home/about', to: 'home#about', as: 'about'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
