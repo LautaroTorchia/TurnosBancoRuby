2e367bcb1b3407776dd5bce0bc960e50f764175e
Lautaro Torchia <torchia.lautaro2@gmail.com>
2022-11-21 13:23:50 -0300

    first style

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module TurnosBanco
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.serve_static_assets = true
   end
 end
